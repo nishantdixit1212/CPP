764e71db39afd47483ed597704739455b7478548
nishantdixit1212 <nishantdixit9719@gmail.com>
2025-10-03 23:20:21 +0530

    Refactor and add various C++ programs
    - Deleted the old accountclass.cpp file and refactored the Account class implementation.
    - Added new arithmetic operations program (allarithmethic.cpp).
    - Created a Hello World program (helloWorld.cpp).
    - Implemented increment and decrement operations (inc+dec.cpp).
    - Added programs to demonstrate sizeof operator for char and int (sizeofchar.cpp, sizeofint.cpp).
    - Created a program to sum two user-input numbers (sumoftwo.cpp).
    - Implemented even/odd check program (even.cpp).
    - Added leap year check program (laepyear.cpp).
    - Created a program to determine grades based on marks (marks.cpp).
    - Implemented voting eligibility check program (vote.cpp).
    - Added factorial calculation program (factorial.cpp).
    - Created a program to print numbers from 1 to 5 using loops (print1to5numberbyloop.cpp).
    - Implemented sum of numbers from 1 to n with a break condition (sumof1ton.cpp).
    - Added a program to sum odd numbers (sumofodd.cpp).
    - Created a while loop program to print numbers from 1 to 50 (while1to50.cpp).
    - Added various pattern printing programs (righttriangle.cpp, square.cpp, triangleinchar.cpp, triangleinnumber.cpp, print123insquare.cpp).
    - Implemented bitwise operations program (bitwise1.cpp).
    - Added decimal to binary conversion function (decimaltobinary.cpp).
    - Created a factorial function program (nfactorial.cpp).
    - Implemented a simple sum function (practice.cpp).
    - Added a program to print prime numbers (primetemprailyquestio.cpp).
    - Created a linear search program (linear.cpp).
    - Deleted unused code file (code.cpp).
    - Added matrix addition program (additionOfMatrix.cpp).

--- opps/accountclass.cpp
+++ opps/accountclass.cpp
@@ -2,50 +2,40 @@
 #include <string>
 using namespace std;
 
-// Account class definition
 class Account {
 private:
     double balance;
     string password;
 
 public:
-    string accountid;
-    string username;
+     string accountid;
+     string username;
 
-    // Setter for balance
-    void setbalance(double b) {
-        balance = b;
-    }
+void setbalance(double b){
+    balance = b;
+}
+double getbalance(){
+    return balance;
+}
 
-    // Getter for balance
-    double getbalance() {
-        return balance;
-    }
+void setpassword(string p){
+    password = p;
+}
 
-    // Setter for password
-    void setpassword(string p) {
-        password = p;
-    }
-
-    // Getter for password (not recommended for real applications)
-    string getpassword() {
-        return password;
-    }
+string getpassword(){
+    return password;
+}
 };
 
-int main() {
-    // Create an Account object
+int main(){
     Account a1;
-
-    // Set account details
     a1.accountid = "9719492213";
     a1.username = "abcdefghijkl";
     a1.setbalance(35000);
     a1.setpassword("123445");
+    
 
-    // Display account ID and balance
-    cout << "Account ID: " << a1.accountid << endl;
-    cout << "Balance: " << a1.getbalance() << endl;
-
+    cout<< a1.accountid << endl;
+    cout<< a1.getbalance();
     return 0;
 }
