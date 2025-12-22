1b828829584b312413efbe806a6e45d788b39c88
nishantdixit1212 <nishantdixit9719@gmail.com>
2025-12-22 15:51:33 +0530

    changetry

---
+++ ch 5 (function)/factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n);
+
+
+int main(){
+    cout<<"factorial is:"<<fact(5);
+
+    return 0;
+}
+
+
+int fact(int n){
+if(n==0){
+    return 1;
+}
+int factNm1=fact(n-1);
+int factN= factNm1*n;
+return factN;
+}
